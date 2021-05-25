1a7884c34f4281246aa256fe7b1c1427da7b8363
Yatsutko Sergey <s3rzh@inbox.ru>
2021-05-25 10:11:48 -0400

    Initial commit

---
+++ src/test/java/com/github/mirocidij/bugtracking/BugTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mirocidij.bugtracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
